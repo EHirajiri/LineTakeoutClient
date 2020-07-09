4714af1f028ac3e8e749554591d5884350b29e2f
Eriko Hirajiri <eriko-hirajiri@greensys.co.jp>
2020-07-09 15:57:58 +0900

    Add LineBotResource.

---
+++ src/main/java/jp/co/greensys/takeout/web/rest/LineBotResource.java
@@ -0,0 +1,27 @@
+package jp.co.greensys.takeout.web.rest;
+
+import java.net.URISyntaxException;
+import java.util.Map;
+import javax.validation.Valid;
+import jp.co.greensys.takeout.service.dto.OrderedDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class LineBotResource {
+    private final Logger log = LoggerFactory.getLogger(LineBotResource.class);
+
+    public LineBotResource() {}
+
+    @PostMapping("/linebot/ordereds")
+    public ResponseEntity<OrderedDTO> createOrdered(@Valid @RequestBody Map orderedDTO) throws URISyntaxException {
+        log.debug("REST request to save Ordered : {}", orderedDTO);
+        return null;
+    }
+}
