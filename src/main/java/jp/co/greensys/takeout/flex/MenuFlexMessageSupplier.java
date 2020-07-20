0fc372e580753a6b33d640284d849a0781666d74
Eriko Hirajiri <eriko-hirajiri@greensys.co.jp>
2020-07-20 17:39:53 +0900

    Modify bot resource.

--- src/main/java/jp/co/greensys/takeout/flex/MenuFlexMessageSupplier.java
+++ src/main/java/jp/co/greensys/takeout/flex/MenuFlexMessageSupplier.java
@@ -2,9 +2,9 @@ package jp.co.greensys.takeout.flex;
 
 import static java.util.Arrays.asList;
 
+import com.linecorp.bot.model.action.Action;
+import com.linecorp.bot.model.action.PostbackAction;
 import com.linecorp.bot.model.action.URIAction;
-import com.linecorp.bot.model.event.EventMode;
-import com.linecorp.bot.model.event.PostbackEvent;
 import com.linecorp.bot.model.message.FlexMessage;
 import com.linecorp.bot.model.message.flex.component.Box;
 import com.linecorp.bot.model.message.flex.component.Button;
@@ -29,23 +29,21 @@ public class MenuFlexMessageSupplier implements Supplier<FlexMessage> {
         final Bubble bubble1 = createBubble(
             "チーズバーガー",
             "500",
-            "https://2.bp.blogspot.com/-V6VfiVDMitg/WC5efOCqmFI/AAAAAAAA_5A/P97lsAxzB5kUDdJYLIz_DvdferdNRl6aACLcB/s400/food_hamburger_cheese.png",
-            false
+            "https://2.bp.blogspot.com/-V6VfiVDMitg/WC5efOCqmFI/AAAAAAAA_5A/P97lsAxzB5kUDdJYLIz_DvdferdNRl6aACLcB/s400/food_hamburger_cheese.png"
         );
         final Bubble bubble2 = createBubble(
             "テリヤキバーガー",
             "600",
-            "https://1.bp.blogspot.com/-ccmRa-W5FdQ/WGnPWhQSnzI/AAAAAAABA4w/krKcel6z1hobC87K1Vj9bG_Me_AfBo15QCLcB/s400/hamburger_teriyaki_burger.png",
-            false
+            "https://1.bp.blogspot.com/-ccmRa-W5FdQ/WGnPWhQSnzI/AAAAAAABA4w/krKcel6z1hobC87K1Vj9bG_Me_AfBo15QCLcB/s400/hamburger_teriyaki_burger.png"
         );
         final Carousel carousel = Carousel.builder().contents(asList(bubble1, bubble2)).build();
         return new FlexMessage("Menu", carousel);
     }
 
-    private Bubble createBubble(String title, String price, String imageURL, Boolean isOutOfStock) {
+    private Bubble createBubble(String title, String price, String imageURL) {
         final Image heroBlock = createHeroBlock(imageURL);
-        final Box bodyBlock = createBodyBlock(title, price, isOutOfStock);
-        final Box footerBlock = createFooterBlock(isOutOfStock);
+        final Box bodyBlock = createBodyBlock(title, price);
+        final Box footerBlock = createFooterBlock();
         return Bubble.builder().hero(heroBlock).body(bodyBlock).footer(footerBlock).build();
     }
 
@@ -59,7 +57,7 @@ public class MenuFlexMessageSupplier implements Supplier<FlexMessage> {
             .build();
     }
 
-    private Box createBodyBlock(String title, String price, Boolean isOutOfStock) {
+    private Box createBodyBlock(String title, String price) {
         final Text titleBlock = Text.builder().text(title).wrap(true).weight(Text.TextWeight.BOLD).size(FlexFontSize.XL).build();
         final Box priceBlock = Box
             .builder()
@@ -68,41 +66,19 @@ public class MenuFlexMessageSupplier implements Supplier<FlexMessage> {
                 asList(Text.builder().text("￥" + price).wrap(true).weight(Text.TextWeight.BOLD).size(FlexFontSize.XL).flex(0).build())
             )
             .build();
-        final Text outOfStock = Text
-            .builder()
-            .text("Temporarily out of stock")
-            .wrap(true)
-            .size(FlexFontSize.XXS)
-            .margin(FlexMarginSize.MD)
-            .color("#FF5551")
-            .build();
 
         FlexComponent[] flexComponents = { titleBlock, priceBlock };
         List<FlexComponent> listComponent = new ArrayList<>(Arrays.asList(flexComponents));
-        if (isOutOfStock) {
-            listComponent.add(outOfStock);
-        }
 
         return Box.builder().layout(FlexLayout.VERTICAL).spacing(FlexMarginSize.SM).contents(listComponent).build();
     }
 
-    private Box createFooterBlock(Boolean isOutOfStock) {
+    private Box createFooterBlock() {
         final Button addToCartEnableButton = Button
             .builder()
             .style(Button.ButtonStyle.PRIMARY)
-            .action(new URIAction("注文する", URI.create("https://apologetic-bunnyhug-41317.herokuapp.com/callback"), null))
+            .action(new PostbackAction("注文する", "type=select&item=001", "チーズバーガー"))
             .build();
-        final Button addToCartDisableButton = Button
-            .builder()
-            .style(Button.ButtonStyle.PRIMARY)
-            .color("#aaaaaa")
-            .action(new URIAction("注文する", URI.create("http://example.com"), null))
-            .build();
-        return Box
-            .builder()
-            .layout(FlexLayout.VERTICAL)
-            .spacing(FlexMarginSize.SM)
-            .contents(asList((!isOutOfStock) ? addToCartEnableButton : addToCartDisableButton))
-            .build();
+        return Box.builder().layout(FlexLayout.VERTICAL).spacing(FlexMarginSize.SM).contents(asList(addToCartEnableButton)).build();
     }
 }
