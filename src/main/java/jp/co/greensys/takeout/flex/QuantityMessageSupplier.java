af4d14a0feebdd26f658776863ccd59595381545
Eriko Hirajiri <eriko-hirajiri@greensys.co.jp>
2020-07-20 18:58:05 +0900

    Modify bot resource.

--- src/main/java/jp/co/greensys/takeout/flex/QuantityMessageSupplier.java
+++ src/main/java/jp/co/greensys/takeout/flex/QuantityMessageSupplier.java
@@ -6,8 +6,10 @@ import com.linecorp.bot.model.action.PostbackAction;
 import com.linecorp.bot.model.message.FlexMessage;
 import com.linecorp.bot.model.message.flex.component.Box;
 import com.linecorp.bot.model.message.flex.component.Button;
+import com.linecorp.bot.model.message.flex.component.Text;
 import com.linecorp.bot.model.message.flex.container.Bubble;
 import com.linecorp.bot.model.message.flex.container.Carousel;
+import com.linecorp.bot.model.message.flex.unit.FlexFontSize;
 import com.linecorp.bot.model.message.flex.unit.FlexLayout;
 import com.linecorp.bot.model.message.flex.unit.FlexMarginSize;
 import java.util.function.Supplier;
@@ -16,17 +18,20 @@ public class QuantityMessageSupplier implements Supplier<FlexMessage> {
 
     @Override
     public FlexMessage get() {
-        Bubble bubble = Bubble.builder().body(createFooterBlock(1, "001")).build();
-        Bubble bubble2 = Bubble.builder().body(createFooterBlock(2, "002")).build();
-        final Carousel carousel = Carousel.builder().contents(asList(bubble, bubble2)).build();
-        return new FlexMessage("Quantity", carousel);
+        Bubble bubble = Bubble.builder().header(createHeroBlock()).body(createFooterBlock("001")).build();
+        return new FlexMessage("Quantity", bubble);
     }
 
-    private Box createFooterBlock(int index, String itemId) {
+    private Box createHeroBlock() {
+        final Text titleBlock = Text.builder().text("title").wrap(true).weight(Text.TextWeight.BOLD).size(FlexFontSize.XL).build();
+        return Box.builder().contents(titleBlock).build();
+    }
+
+    private Box createFooterBlock(String itemId) {
         final Button addToCartEnableButton = Button
             .builder()
             .style(Button.ButtonStyle.PRIMARY)
-            .action(new PostbackAction(index + "個", "type=order&item=" + itemId + "&quantity=" + index, index + "個"))
+            .action(new PostbackAction("注文を確定する", "type=order&item=" + itemId, "注文を確定する"))
             .build();
         return Box.builder().layout(FlexLayout.VERTICAL).spacing(FlexMarginSize.SM).contents(asList(addToCartEnableButton)).build();
     }
