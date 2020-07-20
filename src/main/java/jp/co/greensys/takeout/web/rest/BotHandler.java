fcdeaa37505d224405f0dad617c4a6a7e25362ab
Eriko Hirajiri <eriko-hirajiri@greensys.co.jp>
2020-07-20 15:31:39 +0900

    Modify bot resource.

--- src/main/java/jp/co/greensys/takeout/web/rest/BotHandler.java
+++ src/main/java/jp/co/greensys/takeout/web/rest/BotHandler.java
@@ -1,13 +1,16 @@
 package jp.co.greensys.takeout.web.rest;
 
 import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.FollowEvent;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.PostbackEvent;
+import com.linecorp.bot.model.event.UnfollowEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.Message;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+import jp.co.greensys.takeout.util.QueryStringParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,6 +19,16 @@ public class BotHandler {
     private final Logger log = LoggerFactory.getLogger(BotHandler.class);
 
     public BotHandler() {}
+
+    @EventMapping
+    public void handleFollowEvent(FollowEvent event) {
+        log.debug("handleFollowEvent: {}", event);
+    }
+
+    @EventMapping
+    public void handleUnfollowEvent(UnfollowEvent event) {
+        log.debug("handleUnfollowEvent: {}", event);
+    }
 
     @EventMapping
     public Message handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
@@ -32,5 +45,12 @@ public class BotHandler {
     @EventMapping
     public void handlePostbackEvent(PostbackEvent event) {
         log.debug("handlePostbackEvent: {}", event);
+        QueryStringParser parser = new QueryStringParser(event.getPostbackContent().getData());
+        log.debug("PostbackDataType:{}", parser.getParameterValue("type"));
+        switch (parser.getParameterValue("type")) {
+            case "menu":
+                log.debug("postback menu.");
+                break;
+        }
     }
 }
