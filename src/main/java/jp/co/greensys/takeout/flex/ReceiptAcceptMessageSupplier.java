6cce7ad1d287c9d7fa753c6454123c0c881d5d86
Eriko Hirajiri <eriko-hirajiri@greensys.co.jp>
2020-07-31 18:04:46 +0900

    Modify bot resource.

--- src/main/java/jp/co/greensys/takeout/flex/ReceiptAcceptMessageSupplier.java
+++ src/main/java/jp/co/greensys/takeout/flex/ReceiptAcceptMessageSupplier.java
@@ -2,16 +2,22 @@ package jp.co.greensys.takeout.flex;
 
 import com.linecorp.bot.model.message.FlexMessage;
 import com.linecorp.bot.model.message.flex.component.Box;
+import com.linecorp.bot.model.message.flex.component.FlexComponent;
 import com.linecorp.bot.model.message.flex.component.Text;
 import com.linecorp.bot.model.message.flex.container.Bubble;
 import com.linecorp.bot.model.message.flex.unit.FlexFontSize;
 import com.linecorp.bot.model.message.flex.unit.FlexLayout;
 import com.linecorp.bot.model.message.flex.unit.FlexMarginSize;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.function.Supplier;
+import jp.co.greensys.takeout.service.dto.ItemDTO;
+import jp.co.greensys.takeout.service.dto.OrderItemDTO;
 import jp.co.greensys.takeout.service.dto.OrderedDTO;
 import jp.co.greensys.takeout.util.DateTimeUtil;
 import jp.co.greensys.takeout.util.FlexComponentUtil;
+import org.springframework.util.CollectionUtils;
 
 public class ReceiptAcceptMessageSupplier implements Supplier<FlexMessage> {
     private final OrderedDTO orderedDTO;
@@ -52,33 +58,41 @@ public class ReceiptAcceptMessageSupplier implements Supplier<FlexMessage> {
     }
 
     private Box createFooterBlock() {
-        // 商品情報
-        //        final Text itemBlock = FlexComponentUtil.createText(String.format("商品: %s", orderedDTO.getItemName()), null, FlexFontSize.LG);
-        //        final Text calcBlock = FlexComponentUtil.createText(
-        //            String.format("[%s円 × %s個]", orderedDTO.getUnitPrice(), orderedDTO.getQuantity()),
-        //            "#555555",
-        //            FlexFontSize.SM
-        //        );
-        final Box itemBox = Box.builder().layout(FlexLayout.VERTICAL).contents(Arrays.asList()).build();
+        List<FlexComponent> listComponent = new ArrayList<>();
 
-        // 合計金額
-        final Text totalFeeBlock = FlexComponentUtil.createText(
-            String.format("合計: %s円", orderedDTO.getTotalFee()),
-            null,
-            FlexFontSize.LG
-        );
+        if (!CollectionUtils.isEmpty(orderedDTO.getOrderItems())) {
+            listComponent.add(FlexComponentUtil.createSeparator());
+            // 商品情報
+            for (OrderItemDTO itemDTO : orderedDTO.getOrderItems()) {
+                final Text itemBlock = FlexComponentUtil.createText(String.format("商品: %s", itemDTO.getName()), null, FlexFontSize.LG);
+                final Text totalFeeBlock = FlexComponentUtil.createText(
+                    String.format("金額: %s円 × %s個 = %s", itemDTO.getPrice(), itemDTO.getQuantity(), itemDTO.getTotalFee()),
+                    null,
+                    FlexFontSize.LG
+                );
+                listComponent.add(
+                    Box
+                        .builder()
+                        .layout(FlexLayout.VERTICAL)
+                        .spacing(FlexMarginSize.SM)
+                        .contents(itemBlock, totalFeeBlock, FlexComponentUtil.createSeparator())
+                        .build()
+                );
+            }
+            final Text totalBlock = FlexComponentUtil.createText(
+                String.format("合計金額: %s円", orderedDTO.getTotalFee()),
+                null,
+                FlexFontSize.LG
+            );
+            listComponent.add(totalBlock);
+            final Text deliveryDateBlock = FlexComponentUtil.createText(
+                String.format("受取日時: %s", orderedDTO.getDeliveryDate()),
+                null,
+                FlexFontSize.LG
+            );
+            listComponent.add(deliveryDateBlock);
+        }
 
-        // 受け取り日時
-        final Text deliveryDateBlock = FlexComponentUtil.createText(
-            String.format("受取日時： %s", DateTimeUtil.toString(orderedDTO.getDeliveryDate().toEpochMilli())),
-            null,
-            FlexFontSize.LG
-        );
-        return Box
-            .builder()
-            .layout(FlexLayout.VERTICAL)
-            .spacing(FlexMarginSize.SM)
-            .contents(Arrays.asList(FlexComponentUtil.createSeparator(), itemBox, totalFeeBlock, deliveryDateBlock))
-            .build();
+        return Box.builder().layout(FlexLayout.VERTICAL).spacing(FlexMarginSize.SM).contents(listComponent).build();
     }
 }
